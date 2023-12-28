31073d52515004b18f7f3e7e663a96c2dc4c1385
DEbasta <59838552+DEbasta@users.noreply.github.com>
2023-12-28 04:50:44 +0300

    LAB3_1

--- main2.py
+++ main2.py
@@ -4,7 +4,7 @@ from scipy.optimize import differential_evolution
 from scipy.optimize import linprog
 
 
-class Lab3:
+class Lab3_1:
     I11 = randint(2,5)
     I21 = randint(2,5)
     I31 = randint(2,5)
@@ -19,62 +19,145 @@ class Lab3:
     cost = [randint(70, 300), randint(70, 300)]
 
 
-    fun = [0,0,0]
-    XArr = [[],[],[]]
-    Left = [[],[],[]]
+    fun = [0,0,0,0,0]
+    XArr = [[],[],[],[],[]]
+    Left = [[],[],[],[],[]]
+
+    CF4Det = [1,1,-1]
+    CF5Det = [0,0,0]
+
+    XY1 = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+    XY2 = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+
+    def __init__(self):
+        self.CF(self.profit,[(0, None), (0, None)], 0)
+        self.CF(self.revenue,[(0, None), (0, None)], 1)
+        self.CF(self.cost, [(None, 0), (None, 0)], 2)
+        a = [(self.CF4Det[0] * self.profit[0] + self.CF4Det[1] * self.revenue[0] + self.CF4Det[2] * self.cost[0]),
+             (self.CF4Det[0] * self.profit[0] + self.CF4Det[1] * self.revenue[0] + self.CF4Det[2] * self.cost[0])]
+        self.CF(a,[(0, None), (0, None)], 3)
+        a = [(self.CF5Det[0] * self.profit[0] + self.CF5Det[1] * self.revenue[0] + self.CF5Det[2] * self.cost[0]),
+             (self.CF5Det[0] * self.profit[0] + self.CF5Det[1] * self.revenue[0] + self.CF5Det[2] * self.cost[0])]
+        self.CF(a,[(0, None), (0, None)], 4)
+        self.genCF5()
+        print(self.fun)
+        print(self.XArr)
+        print(self.Left)
+        print(self.CF5Det)
+        self.dots()
+        print(self.XY1)
+        print(self.XY2)
 
 
+    def genCF5(self):
+        R1 = randint(1, 9)
+        R2 = randint(1, 9)
+        R3 = randint(1, 9)
 
-    def CF1(self):
-        obj1 = [-self.profit[0], -self.profit[1]]
+        R1 = R1 / 10
+        R2 = R2 / 10
+        R3 = R3 / 10
+
+        R1 = round(R1, 1)
+        R2 = round(R2, 1)
+        R3 = round(R3, 1)
+
+        SUM = R1 + R2 + R3
+        SUM = round(SUM, 1)
+
+        while (SUM != 1):
+            if (SUM < 1):
+                if ((R1 + 0.1) <= 0.9):
+                    R1 += 0.1
+                    R1 = round(R1, 1)
+                    SUM = R1 + R2 + R3
+                    SUM = round(SUM, 1)
+                if (SUM < 1):
+                    if ((R2 + 0.1) <= 0.9):
+                        R2 += 0.1
+                        R2 = round(R2, 1)
+                        SUM = R1 + R2 + R3
+                        SUM = round(SUM, 1)
+                if (SUM < 1):
+                    if ((R3 + 0.1) <= 0.9):
+                        R3 += 0.1
+                        R3 = round(R3, 1)
+                        SUM = R1 + R2 + R3
+                        SUM = round(SUM, 1)
+            elif (SUM > 1):
+                if ((R1 - 0.1) >= 0.1):
+                    R1 -= 0.1
+                    R1 = round(R1, 1)
+                    SUM = R1 + R2 + R3
+                    SUM = round(SUM, 1)
+                if (SUM > 1):
+                    if ((R2 - 0.1) >= 0.1):
+                        R2 -= 0.1
+                        R2 = round(R2, 1)
+                        SUM = R1 + R2 + R3
+                        SUM = round(SUM, 1)
+                if (SUM > 1):
+                    if ((R3 - 0.1) >= 0.1):
+                        R3 -= 0.1
+                        R3 = round(R3, 1)
+                        SUM = R1 + R2 + R3
+                        SUM = round(SUM, 1)
+            self.CF5Det = [R1, R2, -R3]
+
+    def CF(self, arr, bounds, iter):
+        obj1 = [-arr[0], -arr[1]]
         lhs_ineq = [[self.J1[0], self.J2[0]], [self.J1[1], self.J2[1]], [self.J1[2], self.J2[2]]]
         rhs_ineq = [self.amounts[0], self.amounts[1], self.amounts[2]]
-        bnd = [(0, None), (0, None)]
-        opt1 = linprog(c=obj1, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd)
-        self.fun[0] = -opt1.fun
-        self.XArr[0] = opt1.x
-        self.Left[0] = opt1.ineqlin.residual
+        opt1 = linprog(c=obj1, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bounds)
+        if (opt1.fun == 0):
+            self.fun[iter] = opt1.fun
+        else:
+            self.fun[iter] = -opt1.fun
+        self.XArr[iter] = opt1.x
+        self.Left[iter] = opt1.ineqlin.residual
 
         print(self.amounts)
+        print("  ||  ||   ||")
+        print("  \/  \/   \/")
         print(opt1.ineqlin.residual)
 
-    def CF2(self):
-        obj1 = [-self.revenue[0], -self.revenue[1]]
-        lhs_ineq = [[self.J1[0], self.J2[0]], [self.J1[1], self.J2[1]], [self.J1[2], self.J2[2]]]
-        rhs_ineq = [self.amounts[0], self.amounts[1], self.amounts[2]]
-        bnd = [(0, None), (0, None)]
-        opt1 = linprog(c=obj1, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd)
-        self.fun[1] = -opt1.fun
-        self.XArr[1] = opt1.x
-        self.Left[1] = opt1.ineqlin.residual
+    def dots(self):
+        for i in range(3):
+            self.XY1[i] = [0, self.J1[i]]
+            self.XY2[i] = [self.J2[i], 0]
 
-        print(self.amounts)
-        print(opt1.ineqlin.residual)
+        self.XY1[3] = [0, self.fun[0] * 2 / self.profit[0]]
+        self.XY2[3] = [self.fun[0] * 2 / self.profit[1], 0]
+        self.XY1[4] = [0, self.fun[1] * 2 / self.revenue[0]]
+        self.XY2[4] = [self.fun[1] * 2 / self.revenue[1], 0]
+        self.XY1[5] = [0, self.fun[0] * 2 / self.cost[0]]
+        self.XY2[5] = [self.fun[0] * 2 / self.cost[1], 0]
 
-    def CF3(self):
-        obj1 = [-self.cost[0], -self.cost[1]]
-        lhs_ineq = [[self.J1[0], self.J2[0]], [self.J1[1], self.J2[1]], [self.J1[2], self.J2[2]]]
-        rhs_ineq = [self.amounts[0], self.amounts[1], self.amounts[2]]
-        bnd = [(0, None), (0, None)]
-        opt1 = linprog(c=obj1, A_ub=lhs_ineq, b_ub=rhs_ineq, bounds=bnd)
-        self.fun[2] = -opt1.fun
-        self.XArr[2] = opt1.x
-        self.Left[2] = opt1.ineqlin.residual
+        if (self.fun[3] > self.fun[4]):
+            self.XY1[6] = [0, self.fun[3] * 2 / self.cost[0]]
+            self.XY2[6] = [self.fun[3] * 2 / self.cost[1], 0]
+        elif (self.fun[4] > self.fun[3]):
+            self.XY1[6] = [0, self.fun[4] * 2 / self.cost[0]]
+            self.XY2[6] = [self.fun[4] * 2 / self.cost[1], 0]
 
-        print(self.amounts)
-        print(opt1.ineqlin.residual)
-
-
-
+class Lab3_2:
+    XY1 = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    XY2 = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+    def __init__(self,XY1,XY2):
+        self.XY1 = XY1
+        self.XY2 = XY2
+        print(self.XY1)
 
 
+l1 = Lab3_1()
+XY1 = [Lab3_1.XY1[0], Lab3_1.XY1[1], Lab3_1.XY1[2]]
+XY2 = [Lab3_1.XY2[0], Lab3_1.XY2[1], Lab3_1.XY2[2]]
 
-l = Lab3()
+print("Select CF (1 - 3)")
+# CF = 0
+# input(CF)
+# if (CF == 1):
+#
+l2 = Lab3_2(XY1,XY2)
 
-l.CF1()
-l.CF2()
-l.CF3()
-print(l.fun)
-print(l.XArr)
-print(l.Left)
 
