a74df9c94c3ecee6d9247e91af9bbdf87bc405f2
DEbasta <59838552+DEbasta@users.noreply.github.com>
2023-12-28 06:35:10 +0300

    Lab3

--- main2.py
+++ main2.py
@@ -1,7 +1,9 @@
 from random import random, randint
 
+import matplotlib
 from scipy.optimize import differential_evolution
 from scipy.optimize import linprog
+import matplotlib.pyplot as plt
 
 
 class Lab3_1:
@@ -40,13 +42,14 @@ class Lab3_1:
              (self.CF5Det[0] * self.profit[0] + self.CF5Det[1] * self.revenue[0] + self.CF5Det[2] * self.cost[0])]
         self.CF(a,[(0, None), (0, None)], 4)
         self.genCF5()
-        print(self.fun)
-        print(self.XArr)
-        print(self.Left)
-        print(self.CF5Det)
+        print("L_Opt : ", self.fun)
+        print("X_Opt : ", self.XArr)
+        print("Неравнозначные ЦФ ",self.CF5Det)
         self.dots()
-        print(self.XY1)
-        print(self.XY2)
+        print("Координаты прямых")
+        print("I", self.XY1)
+        print("II", self.XY2)
+        self.draw()
 
 
     def genCF5(self):
@@ -126,12 +129,12 @@ class Lab3_1:
             self.XY1[i] = [0, self.J1[i]]
             self.XY2[i] = [self.J2[i], 0]
 
-        self.XY1[3] = [0, self.fun[0] * 2 / self.profit[0]]
-        self.XY2[3] = [self.fun[0] * 2 / self.profit[1], 0]
-        self.XY1[4] = [0, self.fun[1] * 2 / self.revenue[0]]
-        self.XY2[4] = [self.fun[1] * 2 / self.revenue[1], 0]
-        self.XY1[5] = [0, self.fun[0] * 2 / self.cost[0]]
-        self.XY2[5] = [self.fun[0] * 2 / self.cost[1], 0]
+        self.XY1[3] = [0, self.fun[0] * 2 / self.profit[1]]
+        self.XY2[3] = [self.fun[0] * 2 / self.profit[0], 0]
+        self.XY1[4] = [0, self.fun[1] * 2 / self.revenue[1]]
+        self.XY2[4] = [self.fun[1] * 2 / self.revenue[0], 0]
+        self.XY1[5] = [0, self.fun[0] * 2 / self.cost[1]]
+        self.XY2[5] = [self.fun[0] * 2 / self.cost[0], 0]
 
         if (self.fun[3] > self.fun[4]):
             self.XY1[6] = [0, self.fun[3] * 2 / self.cost[0]]
@@ -139,6 +142,21 @@ class Lab3_1:
         elif (self.fun[4] > self.fun[3]):
             self.XY1[6] = [0, self.fun[4] * 2 / self.cost[0]]
             self.XY2[6] = [self.fun[4] * 2 / self.cost[1], 0]
+
+    def draw(self):
+        plt.plot(self.XY1[0], self.XY2[0], marker='o', label ='J1')
+        plt.plot(self.XY1[1], self.XY2[1], marker='o', label='J2')
+        plt.plot(self.XY1[2], self.XY2[2], marker='o', label='J3')
+        plt.plot(self.XY1[3], self.XY2[3], marker='o', label='ЦФ1')
+        plt.plot(self.XY1[4], self.XY2[4], marker='o', label='ЦФ2')
+        plt.plot(self.XY1[5], self.XY2[5], marker='o', label='ЦФ3')
+        plt.plot(self.XY1[6], self.XY2[6], marker='o', label='Сумм')
+        plt.plot(self.XArr[0], marker='x', label='Реш 1')
+        plt.plot(self.XArr[1], marker='x', label='Реш 2')
+        plt.plot(self.XArr[2], marker='x', label='Реш 3')
+        plt.legend()
+        plt.grid()
+        plt.show()
 
 class Lab3_2:
     XY1 = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
@@ -153,6 +171,9 @@ class Lab3_2:
     fun = [0, 0, 0]
     XArr = [[], [], []]
     Left = [[], [], []]
+    Solutions1 = [0, 0, 0]
+    Solutions2 = [0, 0, 0]
+
 
     concessions = 5
     def __init__(self, XY1, XY2, amounts, J1, J2, profit, revenue, cost):
@@ -170,14 +191,25 @@ class Lab3_2:
             self.CF1(self.profit, [(0, None), (0, None)])
             self.CF2(self.revenue + self.profit, [(0, None), (0, None)])
             self.CF3(self.cost + self.profit + self.revenue, [(0, None), (0, None)])
+            self.dots()
         elif (CF == "2"):
             self.CF1(self.revenue, [(0, None), (0, None)])
             self.CF2(self.profit + self.revenue, [(0, None), (0, None)])
             self.CF3(self.cost + self.revenue + self.profit, [(0, None), (0, None)])
+            self.dots()
         elif (CF == "3"):
             self.CF1(self.cost, [(None, 0), (None, 0)])
             self.CF2(self.profit + self.cost, [(0, None), (0, None)])
             self.CF3(self.revenue + self.cost + self.profit, [(0, None), (0, None)])
+            self.dots()
+
+        print("L_Opt : ", self.fun)
+        print("X_Opt : ", self.XArr)
+        print("Координаты прямых")
+        print("I", self.XY1)
+        print("II", self.XY2)
+
+        self.draw()
 
     def CF1(self, arr, bounds):
         obj1 = [-arr[0], -arr[1]]
@@ -190,10 +222,8 @@ class Lab3_2:
             self.fun[0] = -opt1.fun
         self.XArr[0] = opt1.x
         self.Left[0] = opt1.ineqlin.residual
-
+        print()
         print(self.amounts )
-        print("  ||  ||   ||")
-        print("  \/  \/   \/")
         print(opt1.ineqlin.residual)
 
     def CF2(self, arr, bounds):
@@ -208,9 +238,8 @@ class Lab3_2:
         self.XArr[1] = opt1.x
         self.Left[1] = opt1.ineqlin.residual
 
+        print()
         print(self.amounts ," " ,self.fun[0] - (self.fun[0] / 100 * self.concessions))
-        print("  ||  ||   ||   ||")
-        print("  \/  \/   \/   \/")
         print(opt1.ineqlin.residual)
 
     def CF3(self, arr, bounds):
@@ -226,10 +255,35 @@ class Lab3_2:
         self.XArr[2] = opt1.x
         self.Left[2] = opt1.ineqlin.residual
 
+        print()
         print(self.amounts ," " ,self.fun[0] - (self.fun[0] / 100 * self.concessions), " ", self.fun[1] - (self.fun[1] / 100 * self.concessions))
-        print("  ||  ||   ||   ||")
-        print("  \/  \/   \/   \/")
         print(opt1.ineqlin.residual)
+
+        self.Solutions1 = arr
+        self.Solutions2 = [self.fun[1] - (self.fun[1] / 100 * self.concessions), self.fun[0] - (self.fun[0] / 100 * self.concessions), self.fun[2]]
+
+    def dots(self):
+        self.XY1[3] = [0,  self.Solutions2[0]/self.Solutions1[5]]
+        self.XY2[3] = [self.Solutions2[0] / self.Solutions1[4], 0]
+        self.XY1[4] = [0, self.Solutions2[1] / self.Solutions1[3]]
+        self.XY2[4] = [self.Solutions2[1] / self.Solutions1[2], 0]
+        self.XY1[5] = [0, self.Solutions2[2] / self.Solutions1[1]]
+        self.XY2[5] = [self.Solutions2[2] / self.Solutions1[0], 0]
+
+    def draw(self):
+        plt.plot(self.XY1[0], self.XY2[0], marker='o', label ='J1')
+        plt.plot(self.XY1[1], self.XY2[1], marker='o', label='J2')
+        plt.plot(self.XY1[2], self.XY2[2], marker='o', label='J3')
+        plt.plot(self.XY1[3], self.XY2[3], marker='o', label='ЦФ1')
+        plt.plot(self.XY1[4], self.XY2[4], marker='o', label='ЦФ2')
+        plt.plot(self.XY1[5], self.XY2[5], marker='o', label='ЦФ3')
+        plt.plot(self.XArr[0][0],self.XArr[0][1], marker='x', label='Реш 1')
+        plt.plot(self.XArr[1][0],self.XArr[1][1], marker='x', label='Реш 2')
+        plt.plot(self.XArr[2][0],self.XArr[2][1], marker='x', label='Реш 3')
+        # print(self.XArr)
+        plt.legend()
+        plt.grid()
+        plt.show()
 
 
 
